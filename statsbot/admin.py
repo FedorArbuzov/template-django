4f82f82a2f5b44490dedf3118daea40d5c3e960c
FedorArbuzov <arbuzovfp@gmail.com>
2023-08-31 15:56:32 +0300

    fix

--- statsbot/admin.py
+++ statsbot/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from statsbot.models import Settings, Profile
 
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'username')
+
 
 admin.site.register(Settings)
 admin.site.register(Profile)
