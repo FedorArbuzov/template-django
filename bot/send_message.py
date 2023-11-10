ff93a2685b1c53c868612997477abb31f43c2dc2
FedorArbuzov <arbuzovfp@gmail.com>
2023-11-10 11:20:57 +0300

    fix

--- bot/send_message.py
+++ bot/send_message.py
@@ -64,7 +64,6 @@ def send_start_message(chat_id):
                     'text': settings.channel_btn_text,
                     'callback_data': f'/channel'
                 }],
-                заказчик попросил скрыть
                 [{
                     'text': settings.check_list_btn_text,
                     'callback_data': f'/subscribe_3'
